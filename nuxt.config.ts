48ec5142d68d0ed53b419d5685165f95a5c1eaeb
Michael Thiessen <electromikenetic@gmail.com>
2022-09-26 10:32:43 -0400

    feat: complete Mastering Nuxt 3 course

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,26 @@
+import vsharp from 'vite-plugin-vsharp';
+
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss'],
+  runtimeConfig: {
+    stripeSecret: '',
+    stripeWebhookSecret: '',
+    public: {
+      stripeKey: '',
+    },
+  },
+  nitro: {
+    prerender: {
+      routes: ['/landing'],
+    },
+  },
+  vite: {
+    plugins: [vsharp()],
+  },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@vueuse/nuxt',
+    '@nuxtjs/supabase',
+    '@pinia/nuxt',
+  ],
 });
